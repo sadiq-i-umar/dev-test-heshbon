8b4d107c716aa662771592dd954d6a3f5200b519
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-11-17 15:33:26 +0100

    Fix initial zoom on Samsung Devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <meta name='viewport' content='width=device-width' />
+      </head>
       <body className={openSans.className}>{children}</body>
     </html>
   );
