6ce537f9d28a33626f14b7d32db907b945a51447
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-11-17 16:25:44 +0100

    Fix status item color on management dashboard cards

--- components/card/details/management/index.tsx
+++ components/card/details/management/index.tsx
@@ -10,7 +10,15 @@ const ManagementCardDetails: React.FC<{
           <div style={{ color: '#1B1B1B', fontWeight: 400, fontSize: '14px' }}>
             {detail.title}
           </div>
-          <div style={{ color: '#1B1B1B', fontWeight: 600, fontSize: '15px' }}>
+          <div
+            style={{
+              fontWeight: 600,
+              fontSize: '15px',
+              ...(detail.title === 'Status'
+                ? { color: '#107C41' }
+                : { color: '#1B1B1B' }),
+            }}
+          >
             {detail.body}
           </div>
         </div>
